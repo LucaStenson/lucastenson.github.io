187389758ef7695d4d52f7fdc2c25a35c872aa80
Lycia Stenson <23704896+LucaStenson@users.noreply.github.com>
2025-11-03 16:05:33 +0000

    Added logos

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -3,6 +3,7 @@
 	export let description: string;
 	export let backgroundImage: string | null = null;
 	export let blur: boolean = false;
+	export let logos: string[] = [];
 	export let link: string;
 </script>
 
@@ -14,8 +15,16 @@
 		class="absolute inset-0 bg-cover bg-center duration-200 group-hover:scale-107 filter {blur ? 'blur-sm' : ''}"
 		style="background-image: {backgroundImage ? `url(${backgroundImage})` : 'none'}"
 	></div>
+	
+	<div class="absolute inset-0 bg-black/15 dark:bg-black/30 transition-opacity duration-50 group-hover:opacity-0"></div>
 
-	<div class="absolute inset-0 bg-black/10 dark:bg-black/30 transition-opacity duration-50 group-hover:opacity-0"></div>
+	{#if logos.length > 0}
+	<div class="absolute top-2 left-2 flex gap-2 z-20 p-1 rounded-md bg-white/65">
+		{#each logos as logo}
+		<img src={logo} alt="Logo" class="h-6" />
+		{/each}
+	</div>
+	{/if}
 	
 	<div class="relative pointer-events-none hover-a z-10 space-y-1 p-2 bg-white/65 dark:bg-black/65 rounded-md">
 		<h2 class="text-3xl font-semibold">{title}</h2>
