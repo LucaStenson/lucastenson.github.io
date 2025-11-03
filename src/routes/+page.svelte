d0cbb21cc23e561c5db613458048c7b0f0cda7cd
Lycia Stenson <23704896+LucaStenson@users.noreply.github.com>
2025-11-03 16:32:25 +0000

    Reworded intro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 <div class="space-y-6 text-black dark:text-white">
 	<div class="space-y-4">
 		<p class="text-xl">
-			Hi, I'm a graduate game programmer focused on gameplay, graphics, and engine programming. Enjoys investigating problems and developing efficient solutions that improve designers&apos; workflows, with experience in Unreal, Unity, and Godot, and currently building a custom engine in C++ with Vulkan.
+			Hi, I'm a graduate game programmer focused on gameplay, graphics, and engine programming. I Enjoy investigating problems and developing efficient solutions that improve designers&apos; workflows. I have experience in Unreal, Unity, and Godot, and I'm currently building a custom engine in C++ with Vulkan.
 		</p>
 		<p class="text-xl">
 			Take a look at my favourite projects below.
